249ee0e82c1709f9a5ce0c7f2d3f3d75364a3a5e
pollow <pollow.xc@gmail.com>
2016-02-10 14:23:52 +0800

    Spring Boot starter.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,5 +3,13 @@ package main.java;
 /**
  * Created by Deus@ZJU on 16/2/10.
  */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
